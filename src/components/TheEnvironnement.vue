bd64c501955aca3e3cfbe91360b5e0659516b340
Robin Chappalley <robin@chapi.ch>
2026-02-24 14:04:28 +0100

    Create game environnement

---
+++ src/components/TheEnvironnement.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <a-entity environment="preset: volcano"></a-entity>
+</template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -3,8 +3,7 @@ import { ref } from "vue";
 import "../aframe/clickable.js";
 
 import TheCameraRig from "./TheCameraRig.vue";
-import TheOcean from "./TheOcean.vue";
-import TheIsland from "./TheIsland.vue";
+import TheEnvironnement from "./TheEnvironnement.vue";
 
 const allAssetsLoaded = ref(false);
 const color = ref("#8FD8E3");
@@ -15,17 +14,11 @@ const color = ref("#8FD8E3");
     background="color: black;"
     fog="type: linear; color: #81BEC7; near: 30; far: 250;"
   >
-    <a-assets @loaded="allAssetsLoaded = true">
-      <img
-        id="sky-texture"
-        src="/public/assets/citrus_orchard_puresky.jpg"
-        alt=""
-      />
-    </a-assets>
+    <a-assets @loaded="allAssetsLoaded = true"></a-assets>
 
     <template v-if="allAssetsLoaded">
-      <TheOcean> </TheOcean>
-      <TheIsland> </TheIsland>
+      <TheEnvironnement></TheEnvironnement>
+
     </template>
 
     <TheCameraRig />
